214ee46787845c69a2655dc835876743cd00d73f
fatemeghasemi14 <fateme96gha@gmail.com>
2026-09-14 11:40:40 +0330

    Add Automated Test For Get API And Add Post Api Python File

--- apiValidations.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get("http://216.10.245.166//Library/GetBook.php",
-             params={'AuthorName' : 'Rahul Shetty2'},)
-print(response.text)
-type(response.text)
---
+++ api_validations.py
@@ -0,0 +1,29 @@
+import json
+
+import requests
+
+response = requests.get("http://216.10.245.166//Library/GetBook.php",
+             params={'AuthorName' : 'Rahul Shetty2'},)
+
+# print(response.text)
+# print(type(response.text))
+# dict_response = json.loads(response.text)
+# print(dict_response[0]["isbn"])
+
+json_response = response.json()
+print(type(json_response))
+print(json_response[0]["isbn"])
+
+assert response.status_code == 200
+assert response.headers['Content-Type'] == 'application/json;charset=UTF-8'
+
+# Retrieve the book with ISBN bbcd889
+for actual_book in json_response:
+    if actual_book["isbn"] == "bbcd889":
+        print(actual_book)
+        break
+expected_book = {"book_name":"Learn Appium Automation with Java",
+                 "isbn":"bbcd889",
+                 "aisle":"2200227"}
+
+assert actual_book == expected_book
